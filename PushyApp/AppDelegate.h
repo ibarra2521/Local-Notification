d349c897fd18d61719161deef13716ae65646e76
ibarra2521 <ibarra2521@gmail.com>
2016-01-02 05:18:42 -0500

    Initial Commit

---
+++ PushyApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PushyApp
+//
+//  Created by Nivardo Ibarra on 1/2/16.
+//  Copyright © 2016 Nivardo Ibarra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PushyApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PushyApp
+//
+//  Created by Nivardo Ibarra on 1/2/16.
+//  Copyright © 2016 Nivardo Ibarra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
