881ce08460b7e129e0da8e52cdba52c3e032d0c9
ibarra2521 <ibarra2521@gmail.com>
2016-01-02 08:05:03 -0500

    First version completed

--- PushyApp/ViewController.m
+++ PushyApp/ViewController.m
@@ -9,7 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+-(void) requestPermissionToNotify;
+-(void) createANotification:(int)secondsInTheFuture;
 @end
 
 @implementation ViewController
@@ -22,6 +23,52 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void) requestPermissionToNotify {
+    UIMutableUserNotificationAction *floatAction = [[UIMutableUserNotificationAction alloc] init];
+    floatAction.identifier = @"FLOAT_ACTION";
+    floatAction.title = @"Float";
+    floatAction.activationMode = UIUserNotificationActivationModeBackground;
+    floatAction.destructive = YES;
+    floatAction.authenticationRequired = NO;
+    
+    UIMutableUserNotificationAction *stingAction = [[UIMutableUserNotificationAction alloc] init];
+    stingAction.identifier = @"STING_ACTION";
+    stingAction.title = @"Sting";
+    stingAction.activationMode = UIUserNotificationActivationModeForeground;
+    stingAction.destructive = NO;
+    stingAction.authenticationRequired = NO;
+    
+    UIMutableUserNotificationCategory *responseCategory = [[UIMutableUserNotificationCategory alloc]
+                                                   init];
+    responseCategory.identifier = @"MAIN_CATEGORY";
+    [responseCategory setActions:@[floatAction, stingAction] forContext:UIUserNotificationActionContextDefault];
+    
+    NSSet *categories = [NSSet setWithObjects:responseCategory, nil];
+    
+    UIUserNotificationType types = UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert;
+    UIUserNotificationSettings *settings =[UIUserNotificationSettings settingsForTypes:types categories:categories];
+    [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+}
+
+-(void) createANotification:(int)secondsInTheFuture {
+    UILocalNotification *localNotif = [[UILocalNotification alloc]init];
+    localNotif.fireDate = [[NSDate date] dateByAddingTimeInterval:secondsInTheFuture];
+    localNotif.timeZone = nil;
+    localNotif.alertTitle = @"Alert Title";
+    localNotif.alertBody = @"Alert Body";
+    localNotif.alertAction = @"Okay";
+    localNotif.soundName = UILocalNotificationDefaultSoundName;
+    
+    localNotif.applicationIconBadgeNumber = 4123;
+    localNotif.category = @"MAIN_CATEGORY";
+    [[UIApplication sharedApplication] scheduleLocalNotification:localNotif];
+}
+
+- (IBAction)scheduleTapped:(id)sender {
+    [self requestPermissionToNotify];
+    [self createANotification:15];
 }
 
 @end
